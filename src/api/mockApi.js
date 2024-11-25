f0ceea1eb45c793b4d0662e70d93c2450d92ae1b
Venkatesh S S <99518666+Venkatesh3693@users.noreply.github.com>
2024-11-25 14:17:03 +0530

    Update mockApi.js

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,4 +1,4 @@
-// src/api/mockApi.js
+
 export const mockUsers = [
     { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin', status: true },
     { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'User', status: false },
